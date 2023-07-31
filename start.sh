21501cfd28d3d669e834b4f0c15af86af5f316a7
Vincent Emonet <vincent.emonet@gmail.com>
2023-07-31 10:09:56 +0200

    fix start.sh

--- start.sh
+++ start.sh
@@ -14,25 +14,28 @@ MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 
-if [ -f /app/gunicorn_conf.py ]; then
-    DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
-elif [ -f /app/app/gunicorn_conf.py ]; then
-    DEFAULT_GUNICORN_CONF=/app/app/gunicorn_conf.py
-else
-    DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
-fi
-export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
-export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
+# if [ -f /app/gunicorn_conf.py ]; then
+#     DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
+# elif [ -f /app/app/gunicorn_conf.py ]; then
+#     DEFAULT_GUNICORN_CONF=/app/app/gunicorn_conf.py
+# else
+#     DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
+# fi
+# export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
+# export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
 
-# If there's a prestart.sh script in the /app directory or other path specified, run it before starting
-PRE_START_PATH=${PRE_START_PATH:-/app/prestart.sh}
-echo "Checking for script in $PRE_START_PATH"
-if [ -f $PRE_START_PATH ] ; then
-    echo "Running script $PRE_START_PATH"
-    . "$PRE_START_PATH"
-else
-    echo "There is no script $PRE_START_PATH"
-fi
+# # If there's a prestart.sh script in the /app directory or other path specified, run it before starting
+# PRE_START_PATH=${PRE_START_PATH:-/app/prestart.sh}
+# echo "Checking for script in $PRE_START_PATH"
+# if [ -f $PRE_START_PATH ] ; then
+#     echo "Running script $PRE_START_PATH"
+#     . "$PRE_START_PATH"
+# else
+#     echo "There is no script $PRE_START_PATH"
+# fi
 
-# Start Gunicorn -w $LIBRECHAT_WORKERS --bind 0.0.0.0:8000
-exec gunicorn -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$APP_MODULE"
+# Start Gunicorn -w $LIBRECHAT_WORKERS --bind 0.0.0.0:8000 -c "$GUNICORN_CONF"
+# -w: number of worker processes for handling requests [1]
+# --threads: number of worker threads for handling requests. [1]
+
+exec gunicorn -w $LIBRECHAT_WORKERS -k "$WORKER_CLASS" "$APP_MODULE"
