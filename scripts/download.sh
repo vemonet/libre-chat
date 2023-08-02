7b68229ff2326f5369a15046026dd1ca07236a13
Vincent Emonet <vincent.emonet@gmail.com>
2023-08-02 13:51:35 +0200

    comment ddl 7B, to fix gh action storage issue

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,8 +1,8 @@
 # https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML
-mkdir -p models
-cd models
-wget -N https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q3_K_L.bin
-cd ..
+# mkdir -p models
+# cd models
+# wget -N https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q3_K_L.bin
+# cd ..
 
 # https://public.ukp.informatik.tu-darmstadt.de/reimers/sentence-transformers/v0.2/
 mkdir -p embeddings
