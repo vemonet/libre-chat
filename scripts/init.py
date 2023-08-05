7623f2af2a90e825943bbd88f4a58b51c96e01ed
Vincent Emonet <vincent.emonet@gmail.com>
2023-08-05 21:25:16 +0200

    improve init logs

--- scripts/init.py
+++ scripts/init.py
@@ -2,15 +2,14 @@ import logging
 import os
 import shutil
 
-from libre_chat.conf import parse_conf
-from libre_chat.llm import Llm
+from libre_chat import Llm, log, parse_conf
 
 # Initialize the Llm at docker run (pre-download files if not present, build the vectorstore)
 # Runs before the API to avoid running on multiple workers
 
 logging.basicConfig(level=logging.getLevelName("INFO"))
 
-print("🚀 Initializing the Llm: download files if not present, build vectorstore")
+log.info("🚀 Initializing the Llm: download files if not present, build vectorstore")
 
 # TODO: take conf as arg?
 conf = parse_conf("chat.yml")
@@ -25,6 +24,7 @@ if (
     and os.path.exists(f"/app/models/{default_model}")
     and conf.llm.model_path.endswith(default_model)
 ):
+    log.info("Initializing default model from docker image")
     shutil.move(f"/app/models/{default_model}", conf.llm.model_path)
 
 if (
@@ -32,6 +32,7 @@ if (
     and os.path.exists(f"/app/embeddings/{default_embeddings}")
     and conf.vector.embeddings_path.endswith(default_embeddings)
 ):
+    log.info("Initializing default embeddings from docker image")
     shutil.move(f"/app/embeddings/{default_embeddings}", conf.vector.embeddings_path)
 
 # if len(os.listdir(conf.vector.documents_path)) < 1:
