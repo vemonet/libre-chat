0c19c9161391bfc180285acde8ff385213f442c2
Vincent Emonet <vincent.emonet@gmail.com>
2023-08-08 16:44:28 +0200

    improve svelte setup

--- src/libre_chat/endpoint.py
+++ src/libre_chat/endpoint.py
@@ -88,10 +88,6 @@ class ChatEndpoint(FastAPI):
         )
 
         # TODO: Try with svelte https://github.com/OriginalStefikO/fastapi-svelte-starter/blob/main/main.py
-        # self.mount("/assets", StaticFiles(directory=pkg_resources.resource_filename("libre_chat", "assets")), name="static")
-        # templates = Jinja2Templates(
-        #     directory=pkg_resources.resource_filename("libre_chat", "webapp")
-        # )
         self.mount(
             "/",
             StaticFiles(
@@ -103,6 +99,7 @@ class ChatEndpoint(FastAPI):
         templates = Jinja2Templates(
             directory=pkg_resources.resource_filename("libre_chat", "templates")
         )
+        # Serve UI from index.html directly
         # @self.get("/", response_class=HTMLResponse, include_in_schema=False)
         # def chat_ui(request: Request) -> Any:
         #     return templates.TemplateResponse(
