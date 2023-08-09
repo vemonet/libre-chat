6fcf876501e202371959ef63a859448e66713630
Vincent Emonet <vincent.emonet@gmail.com>
2023-08-09 11:01:32 +0200

    fix gpu build args in action

--- scripts/download.sh
+++ scripts/download.sh
@@ -15,8 +15,12 @@ mkdir -p models
 cd models
 wget -N https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q3_K_L.bin
 
-# Largest 70B in 2 files:
+# 3rd largest 70B q5:
+# wget -N https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGML/resolve/main/llama-2-70b-chat.ggmlv3.q5_K_M.bin
+
+# 2nd largest 70B q6 in 2 files:
 # wget -N https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGML/resolve/main/llama-2-70b-chat.ggmlv3.q6_K.z01
 # wget -N https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGML/resolve/main/llama-2-70b-chat.ggmlv3.q6_K.zip
-# unzip -d llama-2-70b-chat.ggmlv3.q6_K.zip
+# cat llama-2-70b-chat.ggmlv3.q6_K*  >llama-2-70b-chat.ggmlv3.q6.zip
+# unzip -d llama-2-70b-chat.ggmlv3.q6.zip
 cd ..
