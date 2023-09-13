0b4047d28c792a7bf9afd0e89ad531352f06de32
Vincent Emonet <vincent.emonet@gmail.com>
2023-09-13 12:06:29 +0200

    improve gradio to use custom blocks chatbot, and add like button

--- src/libre_chat/ui.py
+++ src/libre_chat/ui.py
@@ -2,25 +2,26 @@
 from collections.abc import Generator
 from queue import Empty, Queue
 from threading import Thread
-from typing import Any, List, Tuple
+from typing import Any
 
 import gradio as gr
 from langchain.callbacks.base import BaseCallbackHandler
 
 from libre_chat.llm import Llm
-from libre_chat.utils import log
 
 
 # https://github.com/gradio-app/gradio/issues/5345
 def gradio_app(llm: Llm) -> Any:
-    def stream(input_text) -> Generator:
+    def stream(input_text, history) -> Generator:
         # Create a Queue
         q = Queue()
         job_done = object()
 
+        print(input_text)
+
         # Create a function to call - this will run in a thread
         def task() -> None:
-            llm.query(input_text, callbacks=[StreamGradioCallback(q)])
+            llm.query(input_text, history, callbacks=[StreamGradioCallback(q)])
             q.put(job_done)
 
         # Create a thread and start the function
@@ -38,26 +39,50 @@ def gradio_app(llm: Llm) -> Any:
             except Empty:
                 continue
 
-    async def get_chatbot_resp(message: str, history: List[Tuple[str, str]]) -> Any:
-        log.debug(f"Running inference for: {message}, with message history: {history}")
-        for next_token, content in stream(message):
-            yield (content)
+    def vote(data: gr.LikeData):
+        # TODO: save vote in a db
+        if data.liked:
+            print("You upvoted this response: " + data.value)
+        else:
+            print("You downvoted this response: " + data.value)
+
+    # async def get_chatbot_resp(message: str, history: List[Tuple[str, str]]) -> Any:
+    #     log.debug(f"Running inference for: {message}, with message history: {history}")
+    #     for next_token, content in stream(message):
+    #         yield (content)
 
     # https://www.gradio.app/guides/creating-a-chatbot-fast
     # https://www.gradio.app/guides/creating-a-custom-chatbot-with-blocks
-    chat = gr.ChatInterface(
-        get_chatbot_resp,
-        chatbot=gr.Chatbot(height=600),
-        textbox=gr.Textbox(placeholder="Ask me anything", container=False, scale=7),
-        title=llm.conf.info.title,
-        description=llm.conf.info.description,
-        theme="soft",
-        examples=llm.conf.info.examples,
-        cache_examples=False,  # Error in GitHub action tests when enabled
-        # retry_btn=None,
-        # undo_btn="Delete Previous",
-        # clear_btn="Clear",
-    )
+    # chat = gr.ChatInterface(
+    #     get_chatbot_resp,
+    #     chatbot=gr.Chatbot(height=600),
+    #     textbox=gr.Textbox(placeholder="Ask me anything", container=False, scale=7),
+    #     title=llm.conf.info.title,
+    #     description=llm.conf.info.description,
+    #     theme="soft",
+    #     examples=llm.conf.info.examples,
+    #     cache_examples=False,  # Error in GitHub action tests when enabled
+    # )
+
+    with gr.Blocks() as chat:
+        chatbot = gr.Chatbot()
+        msg = gr.Textbox()
+        clear = gr.Button("Clear")
+
+        def user(user_message, history):
+            return "", [*history, [user_message, None]]
+
+        def bot(history):
+            print("history!!", history)
+            history[-1][1] = ""
+            for next_token, content in stream(history[-1][0], history):
+                history[-1][1] = content
+                yield history
+
+        msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(bot, chatbot, chatbot)
+        chatbot.like(vote, None, None)
+        clear.click(lambda: None, None, chatbot, queue=False)
+
     return chat.queue()
 
 
